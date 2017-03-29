70540e9fe1f118e9633807b6c736a72b3f00a2d3
Allan Tribe <atribe@ebrlabs.com>
2017-03-29 14:28:22 -0600

    Removed lombok.

--- src/main/java/com/atribe/reproducingbug/ChildObject.java
+++ src/main/java/com/atribe/reproducingbug/ChildObject.java
@@ -2,17 +2,9 @@ package com.atribe.reproducingbug;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString(exclude = "parentObject")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ChildObject {
     private UUID id;
@@ -23,4 +15,52 @@ public class ChildObject {
     private int orderNumber;
     private String orderLabel;
     private String title;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+    public ParentObject getParentObject() {
+        return parentObject;
+    }
+
+    public void setParentObject(final ParentObject parentObject) {
+        this.parentObject = parentObject;
+    }
+
+    public UUID getComponentId() {
+        return componentId;
+    }
+
+    public void setComponentId(final UUID componentId) {
+        this.componentId = componentId;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(final int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getOrderLabel() {
+        return orderLabel;
+    }
+
+    public void setOrderLabel(final String orderLabel) {
+        this.orderLabel = orderLabel;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
 }
--- src/main/java/com/atribe/reproducingbug/ParentObject.java
+++ src/main/java/com/atribe/reproducingbug/ParentObject.java
@@ -1,13 +1,8 @@
 package com.atribe.reproducingbug;
 
-import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -18,9 +13,6 @@ import java.util.UUID;
  * Created by atribe on 12/27/2016.
  * MasterBatchRecord model mapped to ebr.master_batch_records table.
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ParentObject {
     private UUID id;
@@ -53,4 +45,83 @@ public class ParentObject {
         return parentObject;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(final String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(final String productName) {
+        this.productName = productName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(final String companyName) {
+        this.companyName = companyName;
+    }
+
+    public UUID getCompanyLogoImageId() {
+        return companyLogoImageId;
+    }
+
+    public void setCompanyLogoImageId(final UUID companyLogoImageId) {
+        this.companyLogoImageId = companyLogoImageId;
+    }
+
+    public String getRecordNumber() {
+        return recordNumber;
+    }
+
+    public void setRecordNumber(final String recordNumber) {
+        this.recordNumber = recordNumber;
+    }
+
+    public String getRevisionNumber() {
+        return revisionNumber;
+    }
+
+    public void setRevisionNumber(final String revisionNumber) {
+        this.revisionNumber = revisionNumber;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(final LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getJsonString() {
+        return jsonString;
+    }
+
+    public void setJsonString(final String jsonString) {
+        this.jsonString = jsonString;
+    }
+
+    public List<ChildObject> getChildSet() {
+        return childSet;
+    }
+
+    public void setChildSet(final List<ChildObject> childSet) {
+        this.childSet = childSet;
+    }
 }
